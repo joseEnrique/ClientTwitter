cfbbf6cb7789cadc7a5ca1c275c432edde82d279
ACardona <ACardona90@gmail.com>
2012-04-25 10:07:42 +0200

    credentials

---
+++ credentials.py
@@ -0,0 +1,19 @@
+import oauth 
+
+def oauth_req(url, key, secret, http_method="GET", post_body=None,
+        http_headers=None):
+    consumer = oauth.Consumer(key=CONSUMER_KEY, secret=CONSUMER_SECRET)
+    token = oauth.Token(key=key, secret=secret)
+    client = oauth.Client(consumer, token)
+ 
+    resp, content = client.request(
+        url,
+        method=http_method,
+        body=post_body,
+        headers=http_headers,
+        force_auth_header=True
+    )
+return content
+
+
+
