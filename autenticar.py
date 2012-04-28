e0cc9d3366a7f09813400f7871feaee2134862e6
Jose Enrique Ruiz Navarro <donjoseenriqueruiz@gmail.com>
2012-04-28 20:09:36 +0200

    A empezar se dijo

--- autenticar.py
+++ autenticar.py
@@ -1,3 +1,21 @@
 import requests
 from lxml import etree
+import oauth2
+
+
+consumer = oauth.Consumer(key="your-twitter-consumer-key", 
+                          secret="your-twitter-consumer-secret")
+
+
+request_token_url = "http://twitter.com/oauth/request_token"
+
+cliente = oauth.Client(consumer)
+
+resp, content = client.request(request_token_url, "GET")
+
+
+print resp
+print content
+
+
 
