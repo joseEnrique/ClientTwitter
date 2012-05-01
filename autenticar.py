cc32d97c2f50256a673f29c38f99c1f7dc2a348b
Jose Enrique Ruiz Navarro <donjoseenriqueruiz@gmail.com>
2012-05-01 18:02:06 +0200

    En proceso de pruebas

--- autenticar.py
+++ autenticar.py
@@ -3,8 +3,8 @@ from lxml import etree
 import oauth2 as oauth
 import urlparse
 
-consumer_key = 'llave que te da twitter'
-consumer_secret = 'secret que te da twitter'
+consumer_key = 'b494Z6SxFJYOd62SQmMUw'
+consumer_secret = 'ojHT9mXttg7H7XMfNr9Yxkg9ZdrMDjpZQVDDLhWk' #ambas claves son de un twitter mio de quique, hay que hacerlo con el que tenemos de prueba
 
 request_token_url = 'http://twitter.com/oauth/request_token'
 access_token_url = 'http://twitter.com/oauth/access_token'
@@ -21,21 +21,21 @@ resp, content = client.request(request_token_url, "GET")
 
 request_token = dict(urlparse.parse_qsl(content))
 
+print 
+
+
+#accepted = 'n'
+#while accepted.lower() == 'n':
+#    accepted = raw_input('Estas autorizando? (s/n) ')
+#    oauth_verifier = raw_input('Pon tu pin ')
 
 
 
-accepted = 'n'
-while accepted.lower() == 'n':
-    accepted = raw_input('Estas autorizando? (s/n) ')
-    oauth_verifier = raw_input('Pon tu pin ')
+#token = oauth.Token(request_token['oauth_token'],
+#    request_token['oauth_token_secret'])
+#token.set_verifier(oauth_verifier)
+#client = oauth.Client(consumer, token)
 
-
-
-token = oauth.Token(request_token['oauth_token'],
-    request_token['oauth_token_secret'])
-token.set_verifier(oauth_verifier)
-client = oauth.Client(consumer, token)
-
-resp, content = client.request(access_token_url, "POST")
-access_token = dict(urlparse.parse_qsl(content))
+#resp, content = client.request(access_token_url, "POST")
+#access_token = dict(urlparse.parse_qsl(content))
 
